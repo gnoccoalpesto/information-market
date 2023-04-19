9a342631b5e4d801adeab8c6e8ddfd8e5aed3b32
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2023-04-19 22:02:02 +0200

    added scripts for parallel execution using sbatch: each job will execute an instance of info_market.py for a single config; requires virtual environment (venv) and global system variables. main script can accept files, folders and keyworks, same as info_market.py does

---
+++ job_cluster.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+## Resource Request
+#SBATCH --job-name=info_market                      # for squeue
+#SBATCH --output=/home/fcerri/log/out/im_%j.stdout  # output file, %j will be replaced by the Job ID
+#SBATCH --error=/home/fcerri/log/err/im_%j.stderr   # error file
+#SBATCH --partition=Epyc7452                        # the hardware that you want to run on
+# also: Xeon6138
+#SBATCH --qos=long                                  # queue (short, long)
+# also: short
+#SBATCH --ntasks=1                                  # launched by the job; set higher for MPI programs
+#SBATCH --mem=1gb
+#SBATCH --time=2:00:00                              # the time that you want to allocate to your job
+#SBATCH --cpus-per-task=5                           # cores per task on the same machine; set higher for OpenMP programs
+
+
+## Output folder
+
+
+## Dependencies
+source  ${ENV_DIR}/bin/activate
+
+
+## Job
+# possible output redirection
+python3 ${EXEC_FILE} $1 #> /tmp/fcerri/my_program.stdout
+
+
+## Deactivate env
+deactivate
---
+++ main_cluster.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+####################################
+# EXPECTED INPUT (at leat 2)
+# 1 - FOLDER NAME TO CREATE IN TEMP
+# 2: - CONFIG FILES, could be any combination of files and folders
+####################################
+
+
+declare -A SUB_DIR_BEHAVIOR
+	SUB_DIR_BEHAVIOR[n]="naive"
+	SUB_DIR_BEHAVIOR[Nn]="new_naive"
+	SUB_DIR_BEHAVIOR[s]="sceptical"
+	SUB_DIR_BEHAVIOR[Ns]="new_sceptical"
+	SUB_DIR_BEHAVIOR[r]="ranking"
+	SUB_DIR_BEHAVIOR[v]="variable_scepticism"
+	SUB_DIR_BEHAVIOR[Nv]="variable_scepticism"
+	SUB_DIR_BEHAVIOR[t]="wealth_threshold"
+	SUB_DIR_BEHAVIOR[w]="wealth_weighted"
+
+
+usage(){
+    # echo "Usage: $0 <folder name for /tmp/`whoami`> <config files>"
+    echo "Usage: $0 <config files>"
+    echo "Config files can be a combination of files and folders"
+}
+
+
+## INPUT CHECK
+# if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+
+## CREATE OUTPUT FOLDER
+#TODO
+# MY_TEMP_DIR=/tmp/fcerri
+
+
+## INPUT 
+CONFIGS=""
+for arg in $@; do
+    #read all files from a dir
+    if [ -d $arg ]; then
+        c=$(find $arg -name "*.json")
+    else # if-else construct is superior to elif
+        #checks if it is file
+        if [ -f $arg ]; then
+            c=$arg
+        else
+            #checks if a particular keyword for behavior (sub-folder)
+            if [[ -v SUB_DIR_BEHAVIOR[$arg] ]]; then
+                c=$(find $CONFIG_DIR/${SUB_DIR_BEHAVIOR[$arg]} -name "*.json")
+            else
+                echo "Invalid input"
+                exit 1
+            fi
+        fi
+    fi
+    #newline not working, in bash could use \n or IFS ($IFS)
+    CONFIGS="$CONFIGS $c"
+done
+
+
+## RUN
+for file in $CONFIGS; do
+    sbatch $PROJECT_DIR/job_cluster.sh $file
+done
+
+
+## TRANSFER DATA
+#TODO
+    # mv $MY_TEMP_DIR/* $DATA_DIR
+# compress results first if you have many or very large files
+    # should i use rsync?
